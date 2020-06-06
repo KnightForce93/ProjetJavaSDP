cec99d588c96a38ac09233a7acd7c3a532b4d618
Louis Donikian <louis.donikian@gmail.com>
2020-06-07 00:01:45 +0200

    Stat v2

--- Projet_edt/src/Vue/Statistiques.java
+++ Projet_edt/src/Vue/Statistiques.java
@@ -7,19 +7,25 @@ package Vue;
 
 import java.awt.*; 
 import java.awt.event.*; 
+import java.util.ArrayList;
 import javax.swing.*; 
-import org.jfree.chart.*; 
+import org.jfree.chart.*;
 import org.jfree.chart.plot.*; 
 import org.jfree.data.*; 
-import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
  * @author louisdonikian
  */
 public class Statistiques extends JFrame{
-    private JPanel pnl; 
+    private JPanel panel; 
 
+    /**
+     * Permet d'afficher les stats
+     *
+     * @author J-M. DOUDOUX (inspiré de son exemple de code)
+     */
   public Statistiques() { 
     addWindowListener(new WindowAdapter() { 
       public void windowClosing(WindowEvent e) { 
@@ -27,24 +33,62 @@ public class Statistiques extends JFrame{
         System.exit(0); 
       } 
     }); 
-    pnl = new JPanel(new BorderLayout()); 
-    setContentPane(pnl); 
-    setSize(400, 250); 
-
-    DefaultPieDataset pieDataset = new DefaultPieDataset(); 
-    pieDataset.setValue("Valeur1", new Integer(27)); 
-    pieDataset.setValue("Valeur2", new Integer(10)); 
-    pieDataset.setValue("Valeur3", new Integer(50)); 
-    pieDataset.setValue("Valeur4", new Integer(5)); 
-
-    JFreeChart pieChart = ChartFactory.createPieChart("Test camembert", 
-      pieDataset, true, true, true); 
-    ChartPanel cPanel = new ChartPanel(pieChart); 
-    pnl.add(cPanel); 
+    panel = new JPanel(new BorderLayout()); 
+    setContentPane(panel); 
+    setSize(500, 350); 
+    
+    ArrayList<String> str= new ArrayList();
+    String n = "P305,70,2";
+    str.add(n);
+    n = "EM210,100,1";
+    str.add(n);
+    n = "P446,47,2";
+    str.add(n);
+    n = "C110,30,3";
+    str.add(n);
+    n = "G010,80,4";
+    str.add(n);
+    
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset(); 
+    
+    int nbplaces=0;
+    
+    for(String s:str){
+           
+           String[] parts = s.split(",");
+           String nom = parts[0]; 
+           int capacite = Integer.parseInt(parts[1]);
+           String id_site = parts[2]; 
+           String nom_site ="Indéterminé";
+           
+          
+           if(id_site.equals("1")) {
+              nom_site="E1";
+           } 
+           if(id_site.equals("2")) {
+              nom_site="E2";
+           } 
+           if(id_site.equals("3")) {
+              nom_site="E3";
+           } 
+           if(id_site.equals("4")) {
+              nom_site="E4";
+           } 
+           if(id_site.equals("5")) {
+              nom_site="E5";
+           }
+           
+           dataset.addValue(capacite, nom, nom_site);
+           nbplaces+=capacite;
+    }
+   
+    JFreeChart barChart = ChartFactory.createBarChart("Capacité des salles", "Total : " + nbplaces + " places","Nombre de places", dataset, PlotOrientation.VERTICAL, true, true, false); 
+    ChartPanel cPanel = new ChartPanel(barChart); 
+    panel.add(cPanel); 
   } 
 
   public static void main(String args[]) { 
-    Statistiques tpc = new Statistiques(); 
-    tpc.setVisible(true); 
+    Statistiques stat = new Statistiques(); 
+    stat.setVisible(true); 
   } 
 }

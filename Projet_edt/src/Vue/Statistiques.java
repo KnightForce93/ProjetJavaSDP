6c23c7b2cde95f0d4eccabbe15cea730b227d3f5
Louis Donikian <louis.donikian@gmail.com>
2020-06-07 12:22:56 +0200

    Stats v3 + librairies

--- Projet_edt/src/Vue/Statistiques.java
+++ Projet_edt/src/Vue/Statistiques.java
@@ -13,6 +13,7 @@ import org.jfree.chart.*;
 import org.jfree.chart.plot.*; 
 import org.jfree.data.*; 
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
@@ -26,8 +27,10 @@ public class Statistiques extends JFrame{
      *
      * @author J-M. DOUDOUX (inspiré de son exemple de code)
      */
-  public Statistiques() { 
-    addWindowListener(new WindowAdapter() { 
+  public Statistiques(int choix) { 
+    
+      //femeture de la fenetre
+      addWindowListener(new WindowAdapter() { 
       public void windowClosing(WindowEvent e) { 
         dispose(); 
         System.exit(0); 
@@ -37,58 +40,134 @@ public class Statistiques extends JFrame{
     setContentPane(panel); 
     setSize(500, 350); 
     
-    ArrayList<String> str= new ArrayList();
-    String n = "P305,70,2";
-    str.add(n);
-    n = "EM210,100,1";
-    str.add(n);
-    n = "P446,47,2";
-    str.add(n);
-    n = "C110,30,3";
-    str.add(n);
-    n = "G010,80,4";
-    str.add(n);
-    
-    DefaultCategoryDataset dataset = new DefaultCategoryDataset(); 
-    
-    int nbplaces=0;
-    
-    for(String s:str){
-           
-           String[] parts = s.split(",");
-           String nom = parts[0]; 
-           int capacite = Integer.parseInt(parts[1]);
-           String id_site = parts[2]; 
-           String nom_site ="Indéterminé";
-           
-          
-           if(id_site.equals("1")) {
-              nom_site="E1";
-           } 
-           if(id_site.equals("2")) {
-              nom_site="E2";
-           } 
-           if(id_site.equals("3")) {
-              nom_site="E3";
-           } 
-           if(id_site.equals("4")) {
-              nom_site="E4";
-           } 
-           if(id_site.equals("5")) {
-              nom_site="E5";
-           }
-           
-           dataset.addValue(capacite, nom, nom_site);
-           nbplaces+=capacite;
+    if(choix==1) {
+            ArrayList<String> str= new ArrayList();
+            String n = "P305,70,2";
+            str.add(n);
+            n = "EM210,100,1";
+            str.add(n);
+            n = "P446,47,2";
+            str.add(n);
+            n = "C110,30,3";
+            str.add(n);
+            n = "G010,80,4";
+            str.add(n);
+
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset(); 
+
+            int nbplaces=0;
+
+            for(String s:str){
+
+                   String[] parts = s.split(",");
+                   String nom = parts[0]; 
+                   int capacite = Integer.parseInt(parts[1]);
+                   String id_site = parts[2]; 
+                   String nom_site ="Indéterminé";
+
+
+                   if(id_site.equals("1")) {
+                      nom_site="E1";
+                   } 
+                   if(id_site.equals("2")) {
+                      nom_site="E2";
+                   } 
+                   if(id_site.equals("3")) {
+                      nom_site="E3";
+                   } 
+                   if(id_site.equals("4")) {
+                      nom_site="E4";
+                   } 
+                   if(id_site.equals("5")) {
+                      nom_site="E5";
+                   }
+
+                   dataset.addValue(capacite, nom, nom_site);
+                   nbplaces+=capacite;
+            }
+
+            JFreeChart statBAR = ChartFactory.createBarChart("Capacité des salles", "Total : " + nbplaces + " places","Nombre de places", dataset, PlotOrientation.VERTICAL, true, true, false); 
+            ChartPanel statPanel = new ChartPanel(statBAR); 
+            panel.add(statPanel); 
     }
-   
-    JFreeChart barChart = ChartFactory.createBarChart("Capacité des salles", "Total : " + nbplaces + " places","Nombre de places", dataset, PlotOrientation.VERTICAL, true, true, false); 
-    ChartPanel cPanel = new ChartPanel(barChart); 
-    panel.add(cPanel); 
+    if(choix==2) {
+            
+            ArrayList<String> str= new ArrayList();
+            String n = "1";
+            str.add(n);
+            n = "1";
+            str.add(n);
+            n = "3";
+            str.add(n);
+            n = "2";
+            str.add(n);
+            n = "4";
+            str.add(n);
+            n = "6";
+            str.add(n);
+            n = "6";
+            str.add(n);
+            n = "5";
+            str.add(n);
+            n = "2";
+            str.add(n);
+            
+            DefaultPieDataset dataset = new DefaultPieDataset(); 
+            
+            int cm=0;
+            int td=0;
+            int tp=0;
+            int soutien=0;
+            int projet=0;
+            int examen=0;
+            
+            int nb=0;
+            
+            for(String s:str){
+
+                   String[] parts = s.split(",");
+                   int type = Integer.parseInt(parts[0]);
+                   //String cours ="Indéterminé";
+
+                   if(type==1) {
+                      cm++;
+                   } 
+                   if(type==2) {
+                      td++;
+                   } 
+                   if(type==3) {
+                      tp++;
+                   } 
+                   if(type==4) {
+                      soutien++;
+                   } 
+                   if(type==5) {
+                      projet++;
+                   }
+                   if(type==6) {
+                      examen++;
+                   }
+                   nb++;
+            }
+            dataset.setValue("Cours Magistraux " + cm*100/nb + "%", new Integer(cm)); 
+            dataset.setValue("TD " + td*100/nb + "%", new Integer(td)); 
+            dataset.setValue("TP " + tp*100/nb + "%", new Integer(tp)); 
+            dataset.setValue("Soutien " + soutien*100/nb + "%", new Integer(soutien));  
+            dataset.setValue("Projet " + projet*100/nb + "%", new Integer(projet));
+            dataset.setValue("Examen " + examen*100/nb + "%", new Integer(examen)); 
+            
+
+            JFreeChart statCAMEM = ChartFactory.createPieChart("Répartition des matières sur l'année", dataset, true, true, true); 
+            ChartPanel statPanel = new ChartPanel(statCAMEM); 
+            panel.add(statPanel); 
+    }
+    
   } 
 
   public static void main(String args[]) { 
-    Statistiques stat = new Statistiques(); 
-    stat.setVisible(true); 
+    Statistiques bar = new Statistiques(1); 
+    bar.setVisible(true); 
+    Statistiques camembert = new Statistiques(2); 
+    camembert.setVisible(true); 
   } 
 }
